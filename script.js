9372751112f81bb8ee4aafbe9db18be139a3eb71
juliedosher <juliegdosher@lewisu.edu>
2023-11-08 10:15:40 -0600

    test

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("btnGoogleSignIn").addEventListener("click", oauthSignIn);
 document.getElementById("btnSignOut").addEventListener("click", signOut)
 
+var auth2 = null
+
 function oauthSignIn() {
     // Google's OAuth 2.0 endpoint for requesting an access token
     var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
@@ -33,27 +35,31 @@ function oauthSignIn() {
     console.log("reached")
   }
 
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-  
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+function init() {
+  gapi.load('auth2', function() {
+    auth2 = gapi.auth2.init();
+  });
+}
 
-  if (auth2.isSignedIn.get()) {
-    var profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-  }
+
+function signOut() {
+  auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+  auth2 = null
+}
+  
+if (auth2.isSignedIn.get()) {
+  var profile = auth2.currentUser.get().getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+
+  document.getElementById("pProfile").innerText = "Welcome, " + profile.getGivenName();
+} else {
+  document.getElementById("pProfile").innerText = "You are not currently logged in";
+}
